3d9582ad8c466d8005727eafe6c010b0cb2dcc0b
Scott Johnson <44942176+ScottPJohnson@users.noreply.github.com>
2019-02-24 11:06:24 +0000

    Create turtle_race.py

---
+++ turtle_race.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+
+scott = Turtle()
+scott.color('red')
+scott.shape('turtle')
+scott.penup()
+scott.goto(-160, 100)
+scott.pendown()
+
+kate = Turtle()
+kate.color('yellow')
+kate.shape('turtle')
+kate.penup()
+kate.goto(-160, 70)
+kate.pendown()
+
+jack = Turtle()
+jack.color('blue')
+jack.shape('turtle')
+jack.penup()
+jack.goto(-160, 40)
+jack.pendown()
+
+harris = Turtle()
+harris.color('green')
+harris.shape('turtle')
+harris.penup()
+harris.goto(-160, 10)
+harris.pendown()
+
+for movement in range(100):
+    scott.forward(randint(1,5))
+    kate.forward(randint(1,5))
+    jack.forward(randint(1,5))
+    harris.forward(randint(1,5))
