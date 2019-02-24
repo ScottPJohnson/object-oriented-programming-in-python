b0d721448bbde9e671db10b9a4d9db6ae68cfb2a
Scott Johnson <44942176+ScottPJohnson@users.noreply.github.com>
2019-02-24 14:13:28 +0000

    Delete joke.py

--- joke.py
+++
@@ -1,8 +0,0 @@
-def tell(intro, punchline):
-    print("Knock knock")
-    print("Who's there")
-    print(intro)
-    print(intro + " who")
-    print(punchline)
-	
-tell("Atch", "Sounds like you’ve got a cold!")
