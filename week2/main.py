535d39371988482e2e7263022795c88b584e4821
Scott Johnson <44942176+ScottPJohnson@users.noreply.github.com>
2019-02-26 20:19:38 +0000

    Update main.py

--- week2/main.py
+++ week2/main.py
@@ -1,3 +1,8 @@
 from room import Room
 
-kitchen = Room("Kitchen")
+room1 = Room()
+
+room1.set_name("**Kitchen**")
+room1.set_description("A dank and dirty room buzzing with flies")
+
+room1.describe()
